47c4634388816294718336e3be32e2e1881f1e91
李征 <18752574226@163.com>
2023-02-14 15:27:58 +0800

    配置环境

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
 	productionSourceMap: false,
 	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	lintOnSave: false,
+	outputDir: process.env.outputDir,
 	devServer: {
 		open: process.env.VUE_APP_OPEN === 'false' ? false : true, // 自动打开浏览器
 		port: process.env.VUE_APP_PORT, // 前台代理端口号
